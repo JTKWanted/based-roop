0f5ad0901b44153fdabae9f1f6f9a9b4dc6bd5d7
JTKWanted <jtkwanted104@gmail.com>
2025-06-17 03:26:59 -0400

    Update core.py

--- roop/core.py
+++ roop/core.py
@@ -19,7 +19,7 @@ import tensorflow
 
 import roop.globals
 import roop.metadata
-import roop.ui as ui
+# import roop.ui as ui
 from roop.predicter import predict_image, predict_video
 from roop.processors.frame.core import get_frame_processors_modules
 from roop.utilities import has_image_extension, is_image, is_video, detect_fps, create_video, extract_frames, get_temp_frame_paths, restore_audio, create_temp, move_temp, clean_temp, normalize_output_path
@@ -128,8 +128,6 @@ def pre_check() -> bool:
 
 def update_status(message: str, scope: str = 'ROOP.CORE') -> None:
     print(f'[{scope}] {message}')
-    if not roop.globals.headless:
-        ui.update_status(message)
 
 
 def start() -> None:
@@ -200,8 +198,12 @@ def run() -> None:
         if not frame_processor.pre_check():
             return
     limit_resources()
-    if roop.globals.headless:
-        start()
-    else:
-        window = ui.init(start, destroy)
-        window.mainloop()
+def run() -> None:
+    parse_args()
+    if not pre_check():
+        return
+    for frame_processor in get_frame_processors_modules(roop.globals.frame_processors):
+        if not frame_processor.pre_check():
+            return
+    limit_resources()
+    start()
