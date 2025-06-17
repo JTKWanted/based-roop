0716d2b61d8d49a4d714bd85e58a2d3c993c8907
JTKWanted <jtkwanted104@gmail.com>
2025-06-17 03:09:33 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
-#!/usr/bin/env python3
-
 from roop import core
 
-if __name__ == '__main__':
-    core.run()
+# Cargar tu imagen fuente (cara) y destino (video o imagen)
+SOURCE_PATH = '/content/source.jpg'
+TARGET_PATH = '/content/target.mp4'  # Puede ser .jpg, .png o .mp4
+OUTPUT_PATH = '/content/result.mp4'  # o .jpg
+
+# Ejecutar el face swap
+core.run(
+    source_path=SOURCE_PATH,
+    target_path=TARGET_PATH,
+    output_path=OUTPUT_PATH,
+    frame_processors=[],
+    keep_fps=True,
+    many_faces=False
+)
